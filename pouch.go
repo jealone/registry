f03c19f14de7155ec7c9c4b55b646be7aab20160
Arthur Guo <jealone@gmail.com>
2021-03-14 21:19:14 +0800

    fix

--- pouch.go
+++ pouch.go
@@ -1,10 +1,31 @@
 package registry
 
 import (
-	"github.com/jealone/sli4go"
+	"errors"
+
+	"github.com/jealone/goconf"
 	"gopkg.in/yaml.v3"
 )
 
 type (
-	YamlNode = yaml.Node
+	YamlNode   = yaml.Node
+	YamlConfig = goconf.YamlConfig
 )
+
+var (
+	errorEmptyRawBytes = errors.New("buf bytes is nil")
+)
+
+func ParseYamlConfig(buf []byte) (*YamlConfig, error) {
+
+	if 0 == len(buf) {
+		return nil, errorEmptyRawBytes
+	}
+
+	conf := &YamlConfig{}
+	err := goconf.Unmarshal(append(buf, '\n'), conf)
+	if nil != err {
+		return nil, err
+	}
+	return conf, nil
+}
